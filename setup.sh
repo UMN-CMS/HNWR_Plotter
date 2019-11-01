373134cacc44a87c921fd46805762508fd00515a
Jae Sung Kim <jae.sung.kim@cern.ch>
2019-11-01 09:29:01 +0900

    setup for cms2

--- setup.sh
+++ setup.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+export PLOTTER_WORKING_DIR=`pwd`
+
 if [[ $HOSTNAME == "jskim-OptiPlex-9020" ]]; then
 
   #export PLOT_PATH=/home/jskim/cernbox/www/HNWR_13TeV/
@@ -10,25 +12,42 @@ if [[ $HOSTNAME == "jskim-OptiPlex-9020" ]]; then
 
 elif [[ $HOSTNAME == *"lxplus"* ]]; then
 
-	export CMS_PATH=/cvmfs/cms.cern.ch
-	source $CMS_PATH/cmsset_default.sh
-	export SCRAM_ARCH=slc6_amd64_gcc630
-	cd /cvmfs/cms.cern.ch/slc6_amd64_gcc630/cms/cmssw/CMSSW_9_4_4/src/
-	eval `scramv1 runtime -sh`
-	cd -
-	source /cvmfs/cms.cern.ch/slc6_amd64_gcc630/cms/cmssw/CMSSW_9_4_4/external/slc6_amd64_gcc630/bin/thisroot.sh
+  export CMS_PATH=/cvmfs/cms.cern.ch
+  source $CMS_PATH/cmsset_default.sh
+  export SCRAM_ARCH=slc6_amd64_gcc630
+  cd /cvmfs/cms.cern.ch/slc6_amd64_gcc630/cms/cmssw/CMSSW_9_4_4/src/
+  eval `scramv1 runtime -sh`
+  cd -
+  source /cvmfs/cms.cern.ch/slc6_amd64_gcc630/cms/cmssw/CMSSW_9_4_4/external/slc6_amd64_gcc630/bin/thisroot.sh
 
   export PLOT_PATH=$PLOTTER_WORKING_DIR/output/
   export WWW_PLOT_PATH=/eos/user/j/jskim/www/HNWR_13TeV/
   alias pl='rsync -auv --delete-excluded output/'$CATANVERSION' '$WWW_PLOT_PATH
   alias plforce='rsync -auv -I --delete-excluded output/'$CATANVERSION' '$WWW_PLOT_PATH
 
+elif [[ $HOSTNAME == *"cms2"* ]]; then
+
+  #### use cvmfs for root ####
+  export CMS_PATH=/cvmfs/cms.cern.ch
+  source $CMS_PATH/cmsset_default.sh
+  export SCRAM_ARCH=slc6_amd64_gcc700
+  export cmsswrel='cmssw-patch/CMSSW_10_4_0_patch1'
+  cd /cvmfs/cms.cern.ch/$SCRAM_ARCH/cms/$cmsswrel/src
+  echo "@@@@ SCRAM_ARCH = "$SCRAM_ARCH
+  echo "@@@@ cmsswrel = "$cmsswrel
+  echo "@@@@ scram..."
+  eval `scramv1 runtime -sh`
+  cd -
+  source /cvmfs/cms.cern.ch/$SCRAM_ARCH/cms/$cmsswrel/external/$SCRAM_ARCH/bin/thisroot.sh
+
+  export PLOT_PATH=$PLOTTER_WORKING_DIR/output/
+  export WWW_PLOT_PATH=/eos/user/j/jskim/www/HNWR_13TeV/
+  alias pl='rsync -auv --delete-excluded output/'$CATANVERSION' '$WWW_PLOT_PATH
+  alias plforce='rsync -auv -I --delete-excluded output/'$CATANVERSION' '$WWW_PLOT_PATH
 
 fi
 
-export PLOTTER_WORKING_DIR=`pwd`
 export FILE_PATH=$PLOTTER_WORKING_DIR/rootfiles/
-
 export PYTHONPATH=$PYTHONPATH:$PLOTTER_WORKING_DIR/include/
 
 export SCRIPT_DIR=$PLOTTER_WORKING_DIR/script/

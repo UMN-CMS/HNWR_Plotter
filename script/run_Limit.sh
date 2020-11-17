043e260db7ea5c9802bb768001f842397231d149
Jae Sung Kim <jae.sung.kim@cern.ch>
2020-11-17 21:58:43 +0900

    limit script

---
+++ script/run_Limit.sh
@@ -0,0 +1,4 @@
+root -l -b -q "src/Draw_Limit.C(-1, \"NEW_2020_11_10_114246__YearCombined_RecategorizeTest_Fix\")" &> tmp/log_Limit_Combined.log &
+root -l -b -q "src/Draw_Limit.C(2016, \"NEW_2020_11_10_114248__Year2016_RecategorizeTest_Fix\")" &> tmp/log_Limit_2016.log &
+root -l -b -q "src/Draw_Limit.C(2017, \"NEW_2020_11_10_114249__Year2017_RecategorizeTest_Fix\")" &> tmp/log_Limit_2017.log &
+root -l -b -q "src/Draw_Limit.C(2018, \"NEW_2020_11_10_114251__Year2018_RecategorizeTest_Fix\")" &> tmp/log_Limit_2018.log &
